7b6eb3fb96577b08a059955d75571f536735fb94
matYang <uwse@me.com>
2014-04-09 15:22:08 -0400

    updating stuff

--- rules/uwaterloo/terms/exam_schedule.js
+++ rules/uwaterloo/terms/exam_schedule.js
@@ -4,22 +4,18 @@ var utils = require('../../../utils/utils');
 module.exports = function(webot) {
   // Exam Command Rule
   webot.waitRule('wait_course', function(info, next) {
-    var invalid_format_reply = utils.localizedText(webot, 
-        {
-          'en_us' : 'Invalid format. Please follow this format: cs116 math115 econ101',
-          'zh_cn' : '格式不正确，请仿照以下例子：cs116 math115 econ101'
-        })
+    var invalid_format_reply = '格式不正确，请仿照以下例子：cs116 math115 econ101';
     var subjects = info.text.match(/\D+/g);
-    var catalogNums = info.text.match(/\d+/g); 
+    var catalogNums = info.text.match(/\d+/g);
     console.info("Subjects: " + subjects);
     console.info("Course Numbers: " + catalogNums);
     
-    if (subjects.length == 0 || catalogNums.length == 0 || (subjects.length != catalogNums.length)) {
+    if (subjects.length === 0 || catalogNums.length === 0 || (subjects.length !== catalogNums.length)) {
       next(null, invalid_format_reply);
     } else {
       var requestCounter = subjects.length;
-      var titles = new Array();
-      var descriptions = new Array();
+      var titles = [];
+      var descriptions = [];
       for (var i = 0; i < subjects.length; i++) {
         (function(i) {
           var subject = subjects[i].trim();
@@ -29,11 +25,12 @@ module.exports = function(webot) {
             if (data['meta']['message'] == 'No data returned') {
               info.rewait();
               next(null, 'Oops! No match!\nPlease try again.');
-            } else {
+            }
+            else {
               data = data['data'];
               var course = data['course'];
               var descrip = data['sections'];
-              var result = new Array();
+              var result = [];
               result.push(course+":");
               for (var i = 0; i < descrip.length; i++) {
                 var section = descrip[i]['section'];
@@ -44,18 +41,13 @@ module.exports = function(webot) {
                 var location = descrip[i]['location'];
                 var exam = 'Section: '+section+', Location: '+location;
                 result.push(exam);
+                titles.push(course+'\n'+date+' '+day+'\n'+start+' - '+end);
               }
-              titles.push(course+'\n'+date+' '+day+'\n'+start+' - '+end);
               descriptions.push(result.join('\n'));
 
               requestCounter--;
-              if (requestCounter == 0) {
-                var finalDesc = descriptions.join('\n') + 
-                utils.localizedText(webot, 
-                {
-                  'en_us' : '\nWant Calendar View: Click "Read All" below',
-                  'zh_cn' : '\n想查看完整日历，点击下面的“阅读全文”'
-                });
+              if (requestCounter === 0) {
+                var finalDesc = descriptions.join('\n') + '\n想查看完整日历，点击下面的“阅读全文”';
                 var url = 'http://www.uwexam.com';
                 var reply = {
                 title: titles.join('\n\n'),
@@ -63,12 +55,12 @@ module.exports = function(webot) {
                 url: url
                 };
                 next(null, reply);
-              };
-            };
+              }
+            }
           });
         })(i);
 
-      };
+      }
     }
   });
 
@@ -77,12 +69,7 @@ module.exports = function(webot) {
     pattern: /^(exam)$/i,
     handler: function(info) {
       info.wait('wait_course');
-      return utils.localizedText(webot, 
-        {
-          'en_us' : 'Please enter your courses, e.g. cs116 math115 econ101',
-          'zh_cn' : '请输入你的Courses, 比如 cs116 math115 econ101：'
-        }
-      );
+      return '请输入你的Courses, 比如 cs116 math115 econ101：';
     }
   });
 };
--- utils/redis.js
+++ utils/redis.js
@@ -2,14 +2,16 @@ module.exports = function() {
 
 
 	var _initialize = function() {
+		var redis, rtg;
 
 		// fine for both Dev and Prod stage
 		if (process.env.REDISTOGO_URL) {
-		    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-			var redis = require("redis").createClient(rtg.port, rtg.hostname);
+			rtg   = require("url").parse(process.env.REDISTOGO_URL);
+			redis = require("redis").createClient(rtg.port, rtg.hostname);
 			redis.auth(rtg.auth.split(":")[1]);
-		} else {
-		    var redis = require("redis").createClient();
+		}
+		else {
+			redis = require("redis").createClient();
 		}
 		return redis;
 	};
@@ -17,4 +19,4 @@ module.exports = function() {
 	return {
 		initialize: _initialize
 	};
-}()
+}();
